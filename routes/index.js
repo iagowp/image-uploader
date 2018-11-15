f7f04b545490d4938eb5456ead60c4a0c58413ae
iagowp <iago.wp@hotmail.com>
2018-11-15 16:44:16 -0200

    Create working form, add images list to home

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
+const Image = mongoose.model('Image');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res) => {
+  const images = await Image.find();
+  res.render('index', { title: 'Express', images });
 });
 
 module.exports = router;
